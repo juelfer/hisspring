5ac169413003f63c3dcb792f431abdcedce691f8
Juan Elías Fernández <juan.elias.fernandez@gmail.com>
2018-10-23 17:23:35 +0200

    created dao

--- src/main/java/com/geekshubsacademy/hisspringmvc/domain/dao/IPatientDao.java
+++ src/main/java/com/geekshubsacademy/hisspringmvc/domain/dao/IPatientDao.java
@@ -1,4 +1,7 @@
 package com.geekshubsacademy.hisspringmvc.domain.dao;
 
-public interface IPacienteDao {
+import com.geekshubsacademy.hisspringmvc.domain.entities.Patients;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IPatientDao extends CrudRepository<Patients, Long> {
 }

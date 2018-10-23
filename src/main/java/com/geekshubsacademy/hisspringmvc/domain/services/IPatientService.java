ce5015eac8110b09e730216b9fb6e7213107fce1
Juan Elías Fernández <juan.elias.fernandez@gmail.com>
2018-10-23 17:23:46 +0200

    created service

--- src/main/java/com/geekshubsacademy/hisspringmvc/domain/services/IPatientService.java
+++ src/main/java/com/geekshubsacademy/hisspringmvc/domain/services/IPatientService.java
@@ -1,8 +1,14 @@
 package com.geekshubsacademy.hisspringmvc.domain.services;
 
+import com.geekshubsacademy.hisspringmvc.domain.entities.Patients;
 import org.springframework.stereotype.Service;
 
-@Service
-public interface IPatientServiceImpl {
+import java.util.List;
+import java.util.Optional;
 
+@Service
+public interface IPatientService {
+    public List<Patients> listPatients();
+    public void addPatient(Patients patient);
+    public Optional<Patients> showPatient(Long id);
 }
--- src/main/java/com/geekshubsacademy/hisspringmvc/domain/services/impl/IPatientServiceImpl.java
+++ src/main/java/com/geekshubsacademy/hisspringmvc/domain/services/impl/IPatientServiceImpl.java
@@ -1,8 +1,37 @@
 package com.geekshubsacademy.hisspringmvc.domain.services.impl;
 
+import com.geekshubsacademy.hisspringmvc.domain.dao.IPatientDao;
+import com.geekshubsacademy.hisspringmvc.domain.entities.Patients;
+import com.geekshubsacademy.hisspringmvc.domain.services.IPatientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
-public class IPatientService {
+public class IPatientServiceImpl implements IPatientService {
+
+    @Autowired
+    private IPatientDao patientDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Patients> listPatients() {
+        return (List<Patients>) patientDao.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void addPatient(Patients patient) {
+        patientDao.save(patient);
+    }
+
+    @Override
+    public Optional<Patients> showPatient(Long id) {
+        return patientDao.findById(id);
+    }
+
 
 }
